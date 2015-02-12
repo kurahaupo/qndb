fa50125018a4b87acb4109c97fec123cabec7f1b
Martin D Kealey <martin@kurahaupo.gen.nz>
2015-02-12 15:25:06 +1300

    enable non-ascii micron [µm] constant

--- lib/PDF/scale_factors.pm
+++ lib/PDF/scale_factors.pm
@@ -1,5 +1,10 @@
 #!/module/for/perl
 
+use 5.010;
+use strict;
+use warnings;
+use utf8;
+
 ########################################
 # scale factors for sizes for PDF files
 
@@ -7,8 +12,13 @@ package PDF::scale_factors;
 use constant pt => 1;
 use constant px => 2/3;
 use constant in => 72;
-use constant mm => 72 / 25.4;
+use constant ft => in * 12;
+use constant yd => ft * 2;
 
-use export qw( - pt px in mm );
+use constant mm => 72 / 25.4;
+use constant cm => mm * 10;
+use constant 'µm' => mm / 1000;
+
+use export qw( - pt px in ft yd cm mm µm );
 
 1;
