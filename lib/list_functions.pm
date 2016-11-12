a70e105030e8bda5d75733ae42a57d55001a96bc
Martin D Kealey <martin@kurahaupo.gen.nz>
2016-11-12 12:34:50 +1100

    (whitespace)

--- lib/list_functions.pm
+++ lib/list_functions.pm
@@ -8,6 +8,7 @@ sub uniq(@) { my %seen; grep { ! $seen{$_}++ } @_ }
 sub first(@) { @_ ? shift : () }
 sub near($$;$) { my ($a,$b) = @_; my $s = max(abs($a),abs($b)); abs($a-$b) < $s / ($_[2]||1000) }
 sub flatten(@) { map { ref $_ ? @$_ : $_ || () } @_ }
+
 use export qw( - min max sum uniq first near flatten );
 
 1;
