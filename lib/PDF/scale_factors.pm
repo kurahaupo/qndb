32c382029a9c738cdc64004c20f688f46350fe5d
Martin D Kealey <martin@kurahaupo.gen.nz>
2020-09-05 15:21:39 +1000

    allow both µm and μm

--- lib/PDF/scale_factors.pm
+++ lib/PDF/scale_factors.pm
@@ -4,6 +4,7 @@ use 5.010;
 use strict;
 use warnings;
 use utf8;
+no integer; # everything here is floating point
 
 ########################################
 # scale factors for sizes for PDF files
@@ -15,10 +16,11 @@ use constant in => 72;
 use constant ft => in * 12;
 use constant yd => ft * 2;
 
-use constant mm => 72 / 25.4;
+use constant mm => in / 25.4;
 use constant cm => mm * 10;
-use constant 'µm' => mm / 1000;
+use constant 'µm' => mm / 1000;     # micro symbol, deprecated by Unicode v6 in favour of
+use constant 'μm' => mm / 1000;     # lower-case Greek letter mu
 
-use export qw( - pt px in ft yd cm mm µm );
+use export qw( - pt px in ft yd cm mm µm μm );
 
 1;
