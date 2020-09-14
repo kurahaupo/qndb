a8cef3ae7e49f9f3797196b47d9912b71da11991
Martin D Kealey <martin@kurahaupo.gen.nz>
2020-09-14 15:40:38 +1000

    label SQL as SQL::Drupal7::

--- lib/SQL/generic.pm
+++ lib/SQL/generic.pm
@@ -275,26 +275,6 @@ sub fetch_users($) {
                     $k,
                 if $debug;
     }
-    for my $m (@users) {
-        my $sn = delete $m->{surname} || '';
-        my $pn = delete $m->{pref_name} || '';
-        my $gn = delete $m->{given_name} || '';
-        #$pn ||= $gn;
-        #$gn ||= $pn;
-        my $clean_name  = delete $m->{full_name}
-                       || join( ' ', grep { $_ } $gn || $pn, $sn )
-                       || '';
-        my $sort_by_givenname = lc join ' ', grep { $_ } $gn, $sn;
-        my $sort_by_surname   = lc join ' ', grep { $_ } $sn, $gn;
-        my $n = new string_with_components:: $clean_name,
-                                                family_name       => $sn,
-                                                given_name        => $gn,
-                                                pref_name         => $pn,
-                                                sort_by_surname   => $sort_by_surname,
-                                                sort_by_givenname => $sort_by_givenname;
-        $m->{composite_name} = $n;
-        $m->_make_name_sortable($n);
-    }
     print Dumper(\@users);
     return \@users;
 }
